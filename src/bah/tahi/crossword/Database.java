f8f4bf1d5208c90f1f6c0286053fb93b26890d21
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2024-03-29 15:41:56 +0100

    First implementation of Database class is a success & TestDatabase*

--- src/bah/tahi/crossword/Database.java
+++ src/bah/tahi/crossword/Database.java
@@ -2,27 +2,56 @@ package bah.tahi.crossword;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Database {
-	private Connection connexion;
+	private Connection conn;
 
 	public Database() {
 		try {
-			connexion = connectToDB();
+			conn = connectToDB();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private Connection connectToDB() throws SQLException {
-		Connection conn = DriverManager.getConnection("jdbc:mysql:pra-tp6");
-		return conn;
+		String url = "jdbc:mysql://localhost/base_pra_tp6";
+		return DriverManager.getConnection(url, "root", "");
+	}
+
+	public boolean connected() {
+		return conn != null;
 	}
 
 	public Map<Integer, String> availableGrids() {
-		return null;
+		if (!connected()) {
+			return null;
+		}
+
+		try {
+			String gridSQL = "SELECT * FROM Grid";
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(gridSQL);
+
+			Map<Integer, String> grids = new HashMap<>();
+			while (rs.next()) {
+				int numeroGrille = rs.getInt("numero_grille");
+				int largeur = rs.getInt("largeur");
+				int hauteur = rs.getInt("hauteur");
+				String nomGrille = rs.getString("nom_grille") + " (" + hauteur + "x" + largeur + ")";
+				grids.put(numeroGrille, nomGrille);
+			}
+
+			return grids;
+		} catch (SQLException e) {
+			System.out.println("[ERROR] Error in Database.availableGrids()");
+			return new HashMap<>();
+		}
 	}
 
 	public Crossword extractGrid(int puzzleNumber) {
---
+++ src/bah/tahi/crossword/test/TestDatabase.java
@@ -0,0 +1,32 @@
+package bah.tahi.crossword.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import bah.tahi.crossword.Database;
+
+public class TestDatabase {
+
+	private final Database db = new Database();
+
+	@BeforeEach
+	public void testConnection() {
+		assertTrue(db.connected());
+	}
+
+	@Test
+	public void testDataFetching() {
+		testConnection();
+
+		Map<Integer, String> grids = db.availableGrids();
+		String expectedGridName = "Français débutants (7x6)";
+		String actualGridName = grids.get(10);
+
+		assertEquals(expectedGridName, actualGridName);
+	}
+}
